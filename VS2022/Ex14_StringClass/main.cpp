96db0362c6df2ce6dd78bd2114e3859171d35ba4
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-05-16 14:28:04 -0700

    Removing answer from Append().

--- VS2022/Ex14_StringClass/main.cpp
+++ VS2022/Ex14_StringClass/main.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-    홍정모 연구소 https://honglab.co.kr/
+	홍정모 연구소 https://honglab.co.kr/
 */
 
 #include <iostream>
@@ -12,105 +12,105 @@ using namespace std;
 class MyString
 {
 public:
-    MyString()
-    {
-        // 호출 시점 확인
-        cout << "MyString()" << endl;
+	MyString()
+	{
+		// 호출 시점 확인
+		cout << "MyString()" << endl;
 
-        size_ = 1;
-        str_ = new char[size_];
-    }
+		size_ = 1;
+		str_ = new char[size_];
+	}
 
-    MyString(const char *init_str) // init_str이 유효한 메모리라고 가정
-    {
-        cout << "MyString(const char *init_str)" << endl;
+	MyString(const char* init_str) // init_str이 유효한 메모리라고 가정
+	{
+		cout << "MyString(const char *init_str)" << endl;
 
-        // 1. 글자 수 먼저 확인
-        size_ = 0;
-        while (init_str[size_] != '\0')
-        {
-            size_++; // ++ 연산자 안내
-        }
+		// 1. 글자 수 먼저 확인
+		size_ = 0;
+		while (init_str[size_] != '\0')
+		{
+			size_++; // ++ 연산자 안내
+		}
 
-        // 2. 글자 수가 0이 아니면 메모리 할당
-        if (size_ > 0)
-        {
-            str_ = new char[size_];
-        }
+		// 2. 글자 수가 0이 아니면 메모리 할당
+		if (size_ > 0)
+		{
+			str_ = new char[size_];
+		}
 
-        // 3. 복사
-        for (int i = 0; i < size_; i++)
-        {
-            str_[i] = init_str[i];
-        }
-        // memcpy() 사용 가능
-    }
+		// 3. 복사
+		for (int i = 0; i < size_; i++)
+		{
+			str_[i] = init_str[i];
+		}
+		// memcpy() 사용 가능
+	}
 
-    ~MyString()
-    {
-        // 호출 시점 확인
-        cout << "Destructor" << endl;
+	~MyString()
+	{
+		// 호출 시점 확인
+		cout << "Destructor" << endl;
 
-        size_ = 0;
-        if (str_)
-            delete[] str_;
-    }
+		size_ = 0;
+		if (str_)
+			delete[] str_;
+	}
 
-    void Resize(int new_size)
-    {
-        char *new_str = new char[new_size];
+	void Resize(int new_size)
+	{
+		char* new_str = new char[new_size];
 
-        // memcpy() 사용 가능
-        for (int i = 0; i < (new_size < size_ ? new_size : size_); i++)
-        {
-            new_str[i] = str_[i];
-        }
+		// memcpy() 사용 가능
+		for (int i = 0; i < (new_size < size_ ? new_size : size_); i++)
+		{
+			new_str[i] = str_[i];
+		}
 
-        delete[] str_;
-        str_ = new_str;
-        size_ = new_size;
+		delete[] str_;
+		str_ = new_str;
+		size_ = new_size;
 
-        // new_str 지우면 안되요!
-    }
+		// new_str 지우면 안되요!
+	}
 
-    void Print()
-    {
-        for (int i = 0; i < size_; i++)
-        {
-            cout << str_[i];
-        }
-        cout << endl;
-    }
+	void Print()
+	{
+		for (int i = 0; i < size_; i++)
+		{
+			cout << str_[i];
+		}
+		cout << endl;
+	}
 
-    void Append(MyString *app_str) // 같은 타입을 매개변수로 사용 가능
-    {
-        int old_size = size_;
+	void Append(MyString* app_str) // 같은 타입을 매개변수로 사용 가능
+	{
+		int old_size = size_;
 
-        // 다른 멤버 함수 호출 가능 안내
-        Resize(size_ + app_str->size_);
+		// 다른 멤버 함수 호출 가능
+		// Resize(...);
 
-        // 그림을 스스로 그려내서 이해할 수 있도록 도와줘야 함
-        for (int i = old_size; i < size_; i++)
-        {
-            str_[i] = app_str->str_[i - old_size];
-        }
-    }
+		// 중요한 개념
+		for (int i = old_size; i < size_; i++)
+		{
+			// TODO: 복사
+		}
+	}
 
 private:
-    int size_ = 0;        // m_size
-    char *str_ = nullptr; // m_str (여기서는 구글 스타일)
+	int size_ = 0;        // m_size
+	char* str_ = nullptr; // m_str (여기서는 구글 스타일)
 };
 
 int main()
 {
-    // 클래스 기본 문법 안내
+	// 클래스 기본 문법 안내
 
-    MyString str1("ABCDE"); // 생성자 이용
-    MyString str2("123");
+	MyString str1("ABCDE"); // 생성자 이용
+	MyString str2("123");
 
-    str1.Append(&str2); // 주소 넣어줌
+	str1.Append(&str2); // 주소 넣어줌
 
-    str1.Print();
+	str1.Print();
 
-    return 0;
+	return 0;
 }
